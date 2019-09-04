cbf53000b28c439a387e4ee6cc07f3d7d820458c
nikolaevskiy.m <mik639inc@gmail.com>
2019-09-04 15:10:44 +0400

    in progress

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function run() {
   try {
     const context = github.context;
     console.log(context);
+    console.log(context.payload.pull_request);
     
   } catch (error) {
     core.setFailed(error.message);
