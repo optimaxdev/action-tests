edca2113890b133a72bf4d5a8edfca3adfc8bb1f
nikolaevskiy.m <mik639inc@gmail.com>
2019-09-04 23:19:02 +0400

    try to fetch files

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,24 @@ import * as exec from '@actions/exec';
 
 async function run() {
   try {
+    const gitHubToken = core.getInput('token', {required: true});
     const context = github.context;
+    const api = new github.GitHub(gitHubToken);
+    const {owner, repo} = context.repo;
     if (context.payload.pull_request) {
       const {number, changed_files = 0} = context.payload.pull_request;
 
       if (changed_files > 100) {
         exec.exec('yarn && yarn test');
       } else {
-        console.log(context.payload.pull_request.head);
+        const files = await api.pulls.listFiles({
+          owner,
+          repo,
+          pull_number: number
+        });
+
+        console.log(files);
+        
       }
     }
     
