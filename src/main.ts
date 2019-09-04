e8560f97071c997cd893a3c6f8e8c0414a31c51f
nikolaevskiy.m <mik639inc@gmail.com>
2019-09-04 22:46:30 +0400

    in progress

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import * as exec from '@actions/exec';
 
 async function run() {
   try {
     const context = github.context;
-    console.log(context);
-    console.log(context.payload.pull_request);
+    if (context.payload.pull_request) {
+      const {number, changed_files = 0} = context.payload.pull_request;
+
+      if (changed_files > 100) {
+        exec.exec('yarn && yarn test');
+      } else {
+        console.log(context.payload.pull_request.head);
+      }
+    }
     
   } catch (error) {
     core.setFailed(error.message);
